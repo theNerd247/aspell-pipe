16f0b6c828631b0bf72ba8a6268fdf53e5911f5e
Noah Harvey <noah.harvey247@gmail.com>
2025-05-01 12:19:53 -0400

    adds flake.nix

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    haskell-flake.url = "github:srid/haskell-flake";
+  };
+
+  outputs = inputs@{ self, nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = nixpkgs.lib.systems.flakeExposed;
+      imports = [ inputs.haskell-flake.flakeModule ];
+
+      perSystem = { self', pkgs, config, ... }:
+      {
+        haskellProjects.default = 
+        { devShell.enable = false; 
+        };
+
+        devShells.default = pkgs.mkShell
+        { name = "aspell-pipe-dev";
+          inputsFrom = [ config.haskellProjects.default.outputs.devShell ];
+          packages = [
+            (pkgs.aspellWithDicts (d: [d.en]))
+          ];
+        };
+      };
+  };
+}
